6e1f25ad1a8cf20a42ec6f6c0b8582e0b990c0e9
Miguel <mdiazv22@educastillalamancha.es>
2024-01-31 13:41:45 +0100

    meow

--- AntiViruzz/src/main/java/com/mycompany/antiviruzz/antiviruzz.java
+++ AntiViruzz/src/main/java/com/mycompany/antiviruzz/antiviruzz.java
@@ -2,13 +2,13 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
 
-package com.mycompany.mavenproject1;
+package com.mycompany.antiviruzz;
 
 /**
  *
- * @author Antonio
+ * @author b15-04m
  */
-public class Mavenproject1 {
+public class Antiviruzz {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
