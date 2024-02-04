c98abec98919fa01348c3c579a2504a4ecd9144f
AsobrinoM <asobrinom16@educastillalamancha.com>
2024-02-04 15:27:13 +0100

    rawr

--- AntiViruzz/src/main/java/com/mycompany/antiviruzz/antiviruzz.java
+++ AntiViruzz/src/main/java/com/mycompany/antiviruzz/antiviruzz.java
@@ -8,7 +8,7 @@ package com.mycompany.antiviruzz;
  *
  * @author b15-04m
  */
-public class Antiviruzz {
+public class antiviruzz {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
